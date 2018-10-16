9a8f527c1041dc5cff5420c5ff22da5a601bbff2
Marmot McMahon <marmotmcmahon@gmail.com>
2018-10-16 03:17:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ def valid_move?(board, index)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
     else
-      return truel
+      return true
     end
   end
 
