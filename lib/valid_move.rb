87035a523c98f9790afb42e30b13f767814e05f3
Marmot McMahon <marmotmcmahon@gmail.com>
2018-10-16 03:19:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
   def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+    if (array[ind] == " " || array[ind] == "") || array[ind] == nil
       return false
     else
       return true
