db0d75d1c2ae16635e0611cc38df070a1219f661
Marmot McMahon <marmotmcmahon@gmail.com>
2018-10-16 02:45:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  def position_taken?(array, index)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
+  if position_taken?
+    return ""
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
